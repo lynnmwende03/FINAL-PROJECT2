68de065fcec50f226cbd70538a952c85fa491960
lynn mwende <mwendelynn11@gmail.com>
2025-11-15 18:41:51 +0300

    initial commit

---
+++ FINAL-PROJECT2/Amora Bloom/src/components/layout/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./layout.css";
+
+const Header = () => {
+  return (
+    <div className="Birthday-section">
+      <h1 className="Birthday-title">The Birthday Party</h1>
+    </div>
+  );
+};
+
+export default Header;
